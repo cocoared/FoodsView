a4c3b39947eaa167e190ac647c43673a12c7e0da
cocoared <dmm23program@gmail.com>
2022-04-13 13:48:54 +0000

    [Fix]ユーザー側foodsコントローラー

--- app/controllers/public/foods_controller.rb
+++ app/controllers/public/foods_controller.rb
@@ -2,15 +2,32 @@ class Public::FoodsController < ApplicationController
 
   def index
     @foods = RakutenWebService::Ichiba::Item.search(:genreId => '100227')
+    arr = @foods.map {|food| food} #配列の形成
+
+
+    @foods_page = Kaminari.paginate_array(arr).page(params[:page]).per(10)
+
     # @foods = RakutenWebService::Ichiba::Genre[100316].search(keyword:  'Ruby')
     # binding.irb
+    # @food_total = @foods['food_total'].to_f
+    # @page = params['page'].to_i # 一覧ページはGETパラメーターとして受け取る
+    # all_page = (@food_total / 30).ceil
+    # @pages = [*1..all_page]
   end
 
   def show
     # @foods = RakutenWebService::Ichiba::Item.search({itemCode: params[:id]})
     @food_detail = RakutenWebService::Ichiba::Item.search(itemCode: params[:id]).first
-    # @food = Food.new
-    @comments = @food_detail.comments
+    food = Food.find_by(item_code: params[:id])
+
+    if food.present?
+       @comments = food.comments
+    else
+      @comments = []
+    end
+
+
+    @food = Food.new
     @comment = Comment.new
     # @food_exist = Food.find_by(item_code: params[:id]) # Food ga create saretereba sono record wo syutoku !
 
@@ -22,17 +39,17 @@ class Public::FoodsController < ApplicationController
     # binding.irb
   end
 
-  # def create
-  #   food = Food.new(food_params)
-  #   food.item_code = params[:item_code]
-  #   food.genre_id = 1 # FIX ME
-  #   food.save
-  #   redirect_to food_path(params[:item_code])
-  # end
+   def create
+    food = Food.new(food_params)
+    food.item_code = params[:item_code]
+    # food.genre_id = 1 # FIX ME
+    food.save
+    redirect_to food_path(params[:item_code])
+   end
 
-  # private
-  # def food_params
-  #     params.require(:food).permit(:name, :introduction)
-  # end
+  private
+  def food_params
+      params.require(:food).permit(:name, :item_code)
+  end
 
 end

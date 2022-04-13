5007a58f7a56c141f55a316f427916121453f252
cocoared <dmm23program@gmail.com>
2022-04-13 09:25:24 +0000

    [Fix]新規登録画面・ログイン画面 レイアウト

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,21 @@
 class Admin::CommentsController < ApplicationController
   def create
+    food = Food.find(params[:food_id])
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user.id
+    comment.food_id = params[:food_id]
+    comment.save
+    redirect_back(fallback_location: root_path) #直前のページへリダイレクト
   end
 
   def destroy
+    Comment.find_by(id: params[:id], book_id: params[:food_id]).destroy
+    redirect_back(fallback_location: root_path)
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:impression)
+  end
+
 end

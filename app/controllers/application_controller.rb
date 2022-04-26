4f4d2dbdde94d098beeb384421b7f3dc0919a660
cocoared <dmm23program@gmail.com>
2022-04-26 12:14:40 +0000

    [Fix]新規登録後メッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   def after_sign_in_path_for(resource)
     flash[:notice] = 'ログインしました。'
     foods_path
@@ -9,10 +10,6 @@ class ApplicationController < ActionController::Base
   #   stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
   # end
 
-  def after_sign_up_path_for(resource)
-    flash[:notice] = '新規登録が完了しました。'
-    foods_path
-  end
 
   def after_sign_out_path_for(resource)
     flash[:notice] = 'ログアウトしました。'

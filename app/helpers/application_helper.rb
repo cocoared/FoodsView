db9215e7657401bf75967f83337ea00c7116d703
cocoared <dmm23program@gmail.com>
2022-05-20 05:18:54 +0000

    [Add]ヘルパーメソッド

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def score_average(food)
+    fd = Food.find_by(item_code:  food['itemCode'] )
+    if fd
+      score = fd.comments.average(:rate)
+    else
+      score = 0
+    end
+  end
 end
